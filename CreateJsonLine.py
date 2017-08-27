c9ccd7fcab387c8d40825c5ad6d416b79594eef1
ZhangHaotian <zhtpandog@gmail.com>
2017-08-27 11:18:25 -0700

    added progress tracking file saver

--- CreateJsonLine.py
+++ CreateJsonLine.py
@@ -1,7 +1,5 @@
-import json
 import jsonlines
 from nltk.tag import StanfordNERTagger
-import logging
 
 # read the file into memory
 # path_remote = "/home/entitylinking/confluenceRWP/"
@@ -38,7 +36,11 @@ with jsonlines.open("reliefweb_corpus_raw_20160331_eng.jsonl") as reader:
             writer.write(temp_obj)
             counter += 1
             print "finished tagging " + str(counter) + " / 426016 docs"
-            logging.info("finished tagging " + str(counter) + " / 426016 docs")
+            # logging.info("finished tagging " + str(counter) + " / 426016 docs")
+            if counter % 5000 == 0:
+                with open("back/" + str(counter), "w") as fi:
+                    fi.write("ok")
+
 
 # with open("tagged_raw.json", "w") as f:
 #     json.dump(tagged_raw, f)
